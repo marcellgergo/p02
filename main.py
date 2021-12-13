8989233f0aa812c542285ef8645ae75040e0bf93
marcellgergo <71643467+marcellgergo@users.noreply.github.com>
2021-12-13 12:54:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+Írj programot, ami beolvas két számot, majd kiírja:
+a. az összegüket;
+b. különbségüket;
+c. szorzatukat
+'''
+szam = int(input('Kérek egy számot!'))
+szám = int(input('Kérek egy második számot!'))
+print(szam+szám)
+print(szam-szám)
+print(szam*szám) 
